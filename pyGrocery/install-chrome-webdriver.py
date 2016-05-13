53f67731ee51f1407414ecf4e7e8cb23aa12968d
auto <navych@126.com>
2016-05-13 17:34:27 +0800

    This script has bugs. It need debug in the future

--- pyGrocery/install-chrome-webdriver.py
+++ pyGrocery/install-chrome-webdriver.py
@@ -33,66 +33,80 @@ if len(sys.argv) >1 and sys.argv[1] == 'clean':
 	
 
 # Download chromedriver and google_chrome
-print('********************************************************')
-print('Downloading chromedriver and google-chrome ...')
-ret=pexpect.run('mkdir -p %s'%TMP, withexitstatus=True)
-ret=pexpect.run(r'wget -P %s http://chromedriver.storage.googleapis.com/2.21/chromedriver_linux64.zip'%TMP,
-		withexitstatus=True, timeout=3600)
-ret=pexpect.run(r'wget --no-check-certificate  -P %s https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'%TMP,
-		withexitstatus=True, timeout=3600)
-print('Download OK!')
+def download():
+	print('********************************************************')
+	print('Downloading chromedriver and google-chrome ...')
+	ret=pexpect.run('mkdir -p %s'%TMP, withexitstatus=True)
+	ret=pexpect.run(r'wget -P %s http://chromedriver.storage.googleapis.com/2.21/chromedriver_linux64.zip'%TMP,
+			withexitstatus=True, timeout=3600)
+	ret=pexpect.run(r'wget --no-check-certificate  -P %s https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'%TMP,
+			withexitstatus=True, timeout=3600)
+	print('Download OK!')
 
 # Install chromedriver to /usr/bin
-print('********************************************************')
-print('Installing chromedriver...')
-p=pexpect.spawn(r'sudo unzip -d %s %s/chromedriver_linux64.zip'%(DRIVER, TMP))
-index=p.expect([r'password',r'replace',pexpect.EOF])
-if index==0:
-	p.sendline(SUDO_PASSWORD)
-	
-	index=p.expect([r'replace',pexpect.EOF])
+def install_chromedriver():
+	print('********************************************************')
+	print('Installing chromedriver...')
+	p=pexpect.spawn(r'sudo unzip -d %s %s/chromedriver_linux64.zip'%(DRIVER, TMP))
+	index=p.expect([r'password',r'replace',pexpect.EOF])
 	if index==0:
+		p.sendline(SUDO_PASSWORD)
+		
+		index=p.expect([r'replace',pexpect.EOF])
+		if index==0:
+			print('Find chromedriver in %s and replace it.'%DRIVER)
+			p.sendline('y\n')
+			print('chromedriver is installed OK!')
+		elif index==1:
+			print('chromedriver is installed OK!')
+	elif index==1:
 		print('Find chromedriver in %s and replace it.'%DRIVER)
 		p.sendline('y\n')
 		print('chromedriver is installed OK!')
-	elif index==1:
+	elif index==2:
 		print('chromedriver is installed OK!')
-elif index==1:
-	print('Find chromedriver in %s and replace it.'%DRIVER)
-	p.sendline('y\n')
-	print('chromedriver is installed OK!')
-elif index==2:
-	print('chromedriver is installed OK!')
-# Chagen the mode of chromedriver to make it executable.
-p=pexpect.spawn(r'sudo chmod 755 /usr/bin/chromedriver')
-index=p.expect([r'password',pexpect.EOF])
-if index==0:
-	p.sendline(SUDO_PASSWORD)
+	index=p.expect(pexpect.EOF)  # This is to ensure the program is finished
+	
+	# Chagen the mode of chromedriver to make it executable.
+	p=pexpect.spawn(r'sudo chmod 755 /usr/bin/chromedriver')
+	index=p.expect(r'password')
+	if index==0:
+		p.sendline(SUDO_PASSWORD)
 
 # Install google-chrome
-print('********************************************************')
-p=pexpect.spawn(r'sudo apt-get install libxss1 libappindicator1 libindicator7')
-index=p.expect([r'password', pexpect.EOF])
-if index==0:
-	p.sendline(SUDO_PASSWORD)
-	print(r'Installing depending libs ...')
-elif index==1:
-	print(r'Installing depending libs ...')
+def install_chrome():
+	print('********************************************************')
+	# Install depending packages
+	p=pexpect.spawn(r'sudo apt-get install libxss1 libappindicator1 libindicator7')
+	index=p.expect([r'password', pexpect.EOF])
+	if index==0:
+		p.sendline(SUDO_PASSWORD)
+		print(r'Installing depending libs ...')
+	elif index==1:
+		print(r'Installing depending libs ...')
 
-index=p.expect(pexpect.EOF)
-if index==0:
-	print(r'Installing depending libs OK!')
+	index=p.expect([r'Do you want to continue?', pexpect.EOF])
+	if index==0:
+		p.sendline('y\n')
+		print(r'Installing depending libs OK!')
+	elif index==1:
+		print(r'Installing depending libs OK!')
+	index=p.expect(pexpect.EOF)  # This is to ensure the program is finished
 
+	# Install chrome
+	p=pexpect.spawn(r'sudo dpkg -i %s/google-chrome-stable_current_amd64.deb'%TMP)
+	index=p.expect([r'password', pexpect.EOF])
+	if index==0:
+		p.sendline(SUDO_PASSWORD)
+		print(r'Installing google-chrome ...')
+	elif index==1:
+		print(r'Installing google-chrome ...')
 
-p=pexpect.spawn(r'sudo dpkg -i %s/google-chrome-stable_current_amd64.deb'%TMP)
-index=p.expect([r'password', pexpect.EOF])
-if index==0:
-	p.sendline(SUDO_PASSWORD)
-	print(r'Installing google-chrome ...')
-elif index==1:
-	print(r'Installing google-chrome ...')
+	index=p.expect(pexpect.EOF)  # This is to ensure the program is finished
+	if index==0:
+		print(r'Installing google-chrome OK!')
 
-index=p.expect(pexpect.EOF)
-if index==0:
-	print(r'Installing google-chrome OK!')
-
+if not '-l' in sys.argv:
+	download()
+install_chromedriver()	
+install_chrome()
