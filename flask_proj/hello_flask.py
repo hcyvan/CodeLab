6f4aac458e87930d52d7c3e0dc3968745c58ec34
Navy Cheng <navych@126.com>
2016-06-14 10:18:53 +0800

    update

--- flask_proj/hello_flask.py
+++ flask_proj/hello_flask.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from flask import request
+from flask.templating import render_template
 app=Flask(__name__)
-@app.route("/")
-def hello():
-    return "hello world!"
-if __name__ == "__main__":
-    app.run()
+@app.route(r"/",methods=['GET'])
+def app_config():
+    return render_template(r'app_config.html')
+
+@app.route(r"/version_config",methods=['POST'])
+def version_config():
+    gi=[1,2,3]
+    bs=[1,2,3,4]
+    rs=[1,2]
+    return render_template(r'version_config.html')
     
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
