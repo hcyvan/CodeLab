70e0af8fc34470198fe10eb719015e751400005c
hcyvan <navych@126.com>
2016-08-29 15:38:40 +0800

    Queen.java

---
+++ algorithm/Queen.java
@@ -0,0 +1,12 @@
+import static java.lang.System.out;
+public class Queen{
+	private int N;
+	private int [] queen;
+	public void Queen(int N){
+		this.N=N;
+		this.queen=new int[N];
+	}
+	
+	public static void main(String[] args)
+
+}
