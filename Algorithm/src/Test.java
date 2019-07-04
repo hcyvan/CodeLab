b382f7b02827eaeab58eaac15eb35eb4a97e98ea
NavyCheng <navycheng2020@gmail.com>
2019-07-04 13:34:47 +0800

    update

--- Algorithm/src/Test.java
+++ Algorithm/src/Test.java
@@ -1,5 +1,22 @@
+class Solution {
+    public boolean Find(int target, int [][] array) {
+        int i=array.length, j=0;
+        while (i >=0 && j < array[0].length) {
+            if(array[i][j]==target) {
+                return true;
+            } else if(array[i][j] > target) {
+                i--;
+            } else {
+                j++;
+            }
+        }
+        return false;
+    }
+}
+
+
 public class Test {
-    public static void main(String[] str) throws Exception{
-        System.out.println("test");
+    public static void main(String[] args) {
+        System.out.println(1);
     }
 }
--- cmake-build-debug/CMakeFiles/CodeLab.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CodeLab.
-# This may be replaced when dependencies are built.
